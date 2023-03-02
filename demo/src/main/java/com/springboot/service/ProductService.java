21559193df0f48aba95a30c6fab69b5f4db3c8bc
Muddassir <muddassirhussain40@gmail.com>
2023-03-02 16:01:44 +0530

    search api

---
+++ demo/src/main/java/com/springboot/service/ProductService.java
@@ -0,0 +1,12 @@
+package com.springboot.service;
+
+import java.util.List;
+import com.springboot.entity.Product;
+
+
+public interface ProductService {
+
+	List<Product> searchProducts(String query);
+
+	Product createProduct(Product product);
+}
---
+++ demo/src/main/java/com/springboot/service/impl/ProductServiceImpl.java
@@ -0,0 +1,34 @@
+package com.springboot.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.springboot.entity.Product;
+import com.springboot.repository.ProductRepository;
+import com.springboot.service.ProductService;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+	private ProductRepository productRepository;
+
+	public ProductServiceImpl(ProductRepository productRepository) {
+		super();
+		this.productRepository = productRepository;
+	}
+
+	@Override
+	public List<Product> searchProducts(String query) {
+		List<Product> products = productRepository.searchProducts(query);
+
+		return products;
+	}
+
+	@Override
+	public Product createProduct(Product product) {
+		// TODO Auto-generated method stub
+		return productRepository.save(product);
+	}
+
+}
